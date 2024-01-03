83ba036ce4bd2d66fb282f3ac3925e1f68d8a7cd
Khloud-M <khmh45967@gmail.com>
2024-01-03 15:47:32 +0200

    updates

--- js/file.js
+++ js/file.js
@@ -11,13 +11,46 @@ document
       window.location.href = "home.html";
     }
   });
-document.addEventListener("DOMContentLoaded", function () {
-  new Splide("#card-carousel", {
-    perPage: 2,
+// document.addEventListener("DOMContentLoaded", function () {
+//   new Splide("#card-carousel", {
+//     perPage: 2,
+//     breakpoints: {
+//       640: {
+//         perPage: 1,
+//       },
+//     },
+//   }).mount();
+// });
+// splide
+// new Splide(".splide", {
+//   // type: "loop",
+//   perPage: 4,
+//   rewind: true,
+//   breakpoints: {
+//     640: {
+//       perPage: 2,
+//     },
+//   },
+// }).mount();
+var elms = document.getElementsByClassName("splide");
+
+for (var i = 0; i < elms.length; i++) {
+  new Splide(elms[0], {
+    perPage: 4,
+    rewind: true,
     breakpoints: {
       640: {
-        perPage: 1,
+        perPage: 2,
       },
     },
   }).mount();
-});
+  new Splide(elms[1], {
+    perPage: 4,
+    rewind: true,
+    breakpoints: {
+      640: {
+        perPage: 2,
+      },
+    },
+  }).mount();
+}
