939a52ffc920567be2b48b8cb83af74902f55020
Khloud-M <khmh45967@gmail.com>
2024-01-18 20:25:18 +0200

    updates

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/*=============== SWIPER JS ===============*/
+let swiperCards = new Swiper(".card__content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+  focus: 0,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: false,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 5,
+    },
+  },
+});
